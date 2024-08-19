e00db3cec01e608cb421b655c9c2ac84ea090267
Arup Mapa <arupmapa@gmail.com>
2024-08-19 20:30:16 +0530

    push

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,30 +1,52 @@
 const express = require('express');
 const User = require('../models/User')
 const router = express.Router();
-const {body, validationResult} = require('express-validator');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRECT = "@rupm@p@";
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.send("hello world");
 })
-router.post('/',[
-    body('name').isLength({min:3}),
+router.post('/signup', [
+    body('name').isLength({ min: 3 }),
     body('email').isEmail(),
-    body('password').isLength({min:6}),
-],(req,res)=>{
-  const error = validationResult(req);
-  if(!error.isEmpty())
-  {
-    return res.stutas(400).json({error: error.array()});
-  }
-  else{
-    User.creatr({
-        name: req.body.name,
-        email: req.body.emial,
-        password: req.body.password
-    })
-    .then(responce => responce.json())
-    .then(()=>{console.log("user add succesfully")})
-    .catch((err)=>{console.log("falied to push data during invalid input" + err)});
-  }
+    body('password').isLength({ min: 6 }),
+], async (req, res) => {
+    const error = validationResult(req);
+    console.log(req.body);
+    if (!error.isEmpty()) {
+        return res.status(400).json({ error: error.array() });
+    }
+    else {
+        try {
+            let u = await User.findOne(req.body.email);
+            if (!u) {
+                const salt = await bcrypt.genSalt(10);
+                const secrectPassword = await bcrypt.hash(req.body.password, salt);
+                const user = await User.creatr({
+                    name: req.body.name,
+                    email: req.body.emial,
+                    password: secrectPassword
+                })
+                user.save();
+                const userToken = {
+                    user: {
+                        id: user.id
+                    }
+                }
+                const authToken = jwt.sign(userToken, JWT_SECRECT);
+                console.log(authToken);
+                return res.status(200).send("user add successfully");
+            }
+            else {
+                return res.status(400).send("Email already Exits!");
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send("500 Internal Server Error")
+        }
+    }
 })
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,19 @@ const express = require('express');
 const dbConnection = require('./db');
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 dbConnection();
 
 app.use('/api', require('./routes/login'));
 
 app.listen(PORT,()=>{
-    console.log("Server Started");
+    try{
+        console.log("Server Started on port 5000..");
+    }
+    catch(err)
+    {
+        console.log("unable to start server: " + err);
+        return res.status(500).send(`500 Internal Server Error`);
+    }
 })
