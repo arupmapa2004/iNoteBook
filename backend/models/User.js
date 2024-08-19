00dc7d67677afc6efcb7149924a854fbf7ced047
Arup Mapa <arupmapa@gmail.com>
2024-08-19 18:38:34 +0530

    push

--- backend/models/User.js
+++ backend/models/User.js
@@ -20,5 +20,5 @@ const UserSchema = new Schema({
         default: Date.now
     }
 })
-
-module.exports = mongoose.model('user',UserSchema);
+const User = mongoose.model('user',UserSchema);
+module.exports = User;
--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const User = require('../models/User')
 const router = express.Router();
+const {body, validationResult} = require('express-validator');
 
 router.get('/',(req,res)=>{
     res.send("hello world");
 })
-
+router.post('/',[
+    body('name').isLength({min:3}),
+    body('email').isEmail(),
+    body('password').isLength({min:6}),
+],(req,res)=>{
+  const error = validationResult(req);
+  if(!error.isEmpty())
+  {
+    return res.stutas(400).json({error: error.array()});
+  }
+  else{
+    User.creatr({
+        name: req.body.name,
+        email: req.body.emial,
+        password: req.body.password
+    })
+    .then(responce => responce.json())
+    .then(()=>{console.log("user add succesfully")})
+    .catch((err)=>{console.log("falied to push data during invalid input" + err)});
+  }
+})
 module.exports = router;
