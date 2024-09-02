7e8ab0104d0aa799297d0ad71c59e0521e6ca6ee
Arup Mapa <arupmapa@gmail.com>
2024-09-02 20:25:32 +0530

    push

--- src/components/Addnote.js
+++ src/components/Addnote.js
@@ -1,33 +1,33 @@
-import React, {useContext,useState} from "react";
+import React, { useContext, useState } from "react";
 import noteContext from "../context/notes/noteContext";
 
 function Addnote() {
     const context = useContext(noteContext);
-    const {addnote} = context;
-    const [note,setNote] = useState({title:"", description:"", tag:""});
+    const { addnote } = context;
+    const [note, setNote] = useState({ title: "", description: "", tag: "" });
 
-    const onChange = (e)=>{
-        setNote({...note, [e.target.name] : e.target.value});
+    const onChange = (e) => {
+        setNote({ ...note, [e.target.name]: e.target.value });
     }
-    const onClick = (e)=>{
+    const onClick = (e) => {
         e.preventDefault();
         addnote(note.title, note.description, note.tag);
-        setNote({...note, [e.target.name] : ''});
+        setNote({ title: "", description: "", tag: "" });
     }
     return (
         <div className="container">
-            <form>
+            <form className="row g-3 needs-validation" novalidate>
                 <div className="mb-3">
                     <label htmlFor="title" className="form-label">Title</label>
-                    <input type="text" className="form-control" id="notetitle" name="title" aria-describedby="emailHelp" onChange={onChange} required/>
+                    <input type="text" className="form-control" id="notetitle" name="title" aria-describedby="emailHelp" value={note.title} onChange={onChange} required />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="description" className="form-label">Description</label>
-                    <input type="text" className="form-control" id="notedescription" name="description" onChange={onChange} required/>
+                    <input type="text" className="form-control" id="notedescription" name="description" value={note.description} onChange={onChange} required />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="tag" className="form-label">Tag</label>
-                    <input type="text" className="form-control" id="notetag" name="tag" onChange={onChange} required/>
+                    <input type="text" className="form-control" id="notetag" name="tag" value={note.tag} onChange={onChange} required />
                 </div>
                 <div className="container my-3">
                     <button type="submit" className="btn btn-primary" onClick={onClick}>Add Note</button>
